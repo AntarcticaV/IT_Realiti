ffe16acb868ef0f543cb2fd8ba571110f7f6fbad
AntarcticaV <beliyroman@outlook.com>
2023-03-14 21:55:24 +0700

    MD

---
+++ MD/ICS/ICS/ICS/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using ICS.Windows;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ICS
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnButtonClickedSigIn(object sender, System.EventArgs e)
+        {
+            await Navigation.PushAsync(new Tabbed());
+        }
+    }
+}
